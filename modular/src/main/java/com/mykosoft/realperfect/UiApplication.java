1dbd971c7e941f4d719bf71e79764bd8d9cd7cce
Oleh <oleh@oleh.com>
2020-07-05 12:43:20 +0300

    Update jdbc connector version and make app launching

--- modular/src/main/java/com/mykosoft/realperfect/UiApplication.java
+++ modular/src/main/java/com/mykosoft/realperfect/UiApplication.java
@@ -1,16 +1,8 @@
 package com.mykosoft.realperfect;
 
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import com.mykosoft.realperfect.auth.UserDetailsServiceImpl;
-import com.mykosoft.realperfect.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.mykosoft.realperfect.repository")
-@ComponentScan
 @Controller
 public class UiApplication {
     // Match everything without a suffix (so not a static resource)
